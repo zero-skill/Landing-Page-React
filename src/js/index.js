35596d5727fc88fc6dc7b679e3fb7211d9b55a20
zeroSkill <gabbriel.amaya@gmail.com>
2021-06-07 15:00:32 -0400

    renderizamos la app con sus componentes en orden

--- src/js/index.js
+++ src/js/index.js
@@ -12,6 +12,47 @@ import "../styles/index.scss";
 
 //import your own components
 import { Navbar } from "./component/navbar.js";
+import { Card } from "./component/card.js";
 
+const App = () => {
+	return (
+		<>
+			<Navbar />
+			<div className="container">
+				<div className="row">
+					{/*Este div para almacenar las cartas*/}
+					<Card
+						imgUrl="https://via.placeholder.com/150"
+						title="Card Title"
+						textDescription="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus mauris sapien, porttitor a tincidunt sed, dapibus quis lorem."
+						link="#"
+						textLink="Find Out More!!"
+					/>
+					<Card
+						imgUrl="https://via.placeholder.com/150"
+						title="Card Title"
+						textDescription="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus mauris sapien, porttitor a tincidunt sed, dapibus quis lorem."
+						link="#"
+						textLink="Find Out More!!"
+					/>
+					<Card
+						imgUrl="https://via.placeholder.com/150"
+						title="Card Title"
+						textDescription="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus mauris sapien, porttitor a tincidunt sed, dapibus quis lorem."
+						link="#"
+						textLink="Find Out More!!"
+					/>
+					<Card
+						imgUrl="https://via.placeholder.com/150"
+						title="Card Title"
+						textDescription="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus mauris sapien, porttitor a tincidunt sed, dapibus quis lorem."
+						link="#"
+						textLink="Find Out More!!"
+					/>
+				</div>
+			</div>
+		</>
+	);
+};
 //render your react application
-ReactDOM.render(<Navbar />, document.querySelector("#app"));
+ReactDOM.render(<App />, document.querySelector("#app"));
