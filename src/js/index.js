e99d7391352fcaaaba5e1214a21d25fa55856a45
Gabriel Amaya <gabbriel.amaya@gmail.com>
2021-06-07 16:47:25 +0000

    navbar ready

--- src/js/index.js
+++ src/js/index.js
@@ -3,13 +3,17 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 //include bootstrap npm library into the bundle
+import "jquery";
+import "popper.js";
 import "bootstrap";
 
 //include your index.scss file into the bundle
 import "../styles/index.scss";
 
 //import your own components
-import { Home } from "./component/home.js";
+import { Navbar } from "./component/navbar.js";
+//import { Home } from "./component/home.js";
 
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+ReactDOM.render(<Navbar />, document.querySelector("#app"));
+//ReactDOM.render(<Home />, document.querySelector("#app"));
