4805bc9c26c6919e687fcbee6cf58cf9b3d7226c
arielstark1 <arielmarvin@gmail.com>
2021-06-07 19:57:05 +0000

    cambiosariel

--- src/js/component/footer.js
+++ src/js/component/footer.js
@@ -1,18 +1,19 @@
 import React from "react";
 function Footer() {
-    return (
-
-        <div className="Footer">
-            <div class="card text-white bg-dark mb-12" style="max-width: 1000rem;">
-                <div class="card-body">
-                    <h5 class="card-title">copyRight @ Your website 2021 </h5>
-                    <p class="card-text"></p>
-                </div>
-            </div>
-        </div>
-    );
+	return (
+		<div className="Footer">
+			<div
+				className="card text-white bg-dark mb-12"
+				style={{ maxWidth: "1000rem" }}>
+				<div className="card-body">
+					<h5 className="card-title">
+						copyRight @ Your website 2021{" "}
+					</h5>
+					<p className="card-text"></p>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default Footer;
-
-
