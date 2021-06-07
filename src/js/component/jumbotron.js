2a69776ad68eeecc801b1ab102825af88aeb0adf
zeroSkill <gabbriel.amaya@gmail.com>
2021-06-07 16:07:22 -0400

    ready

--- src/js/component/jumbotron.js
+++ src/js/component/jumbotron.js
@@ -9,17 +9,6 @@ function Jumbotron() {
 					component for calling extra attention to featured content or
 					information.
 				</p>
-<<<<<<< HEAD
-				<hr class="my-4">
-					<p>
-						This is the website for 4 GEEKS the page have a lot of
-						information for developments{" "}
-					</p>
-					<a class="btn btn-primary btn-lg" href="#" role="button">
-						Call to action
-					</a>
-				</hr>
-=======
 				<hr className="my-4" />
 				<p>
 					This is the website for 4 GEEKS the page have a lot of
@@ -28,15 +17,9 @@ function Jumbotron() {
 				<a className="btn btn-primary btn-lg" href="#" role="button">
 					Call to action
 				</a>
->>>>>>> 4805bc9c26c6919e687fcbee6cf58cf9b3d7226c
 			</div>
 		</div>
 	);
 }
 
 export default Jumbotron;
-<<<<<<< HEAD
-
-ReactDOM.render(<Navbar />, document.querySelector("#app"));
-=======
->>>>>>> 4805bc9c26c6919e687fcbee6cf58cf9b3d7226c
