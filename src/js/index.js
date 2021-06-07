e020b2d20f60f936e1aee625ef2e51f2838dad1c
Gabriel Amaya <gabbriel.amaya@gmail.com>
2021-06-07 16:11:19 +0000

    navbar ready

--- src/js/index.js
+++ src/js/index.js
@@ -9,7 +9,7 @@ import "bootstrap";
 import "../styles/index.scss";
 
 //import your own components
-import { Home } from "./component/home.js";
+import { Navbar } from "./component/navbar.js";
 
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+ReactDOM.render(<Navbar />, document.querySelector("#app"));
